1480e6af30d4d7ccf0a26c5aa738464440e7b1aa
Jack Kelley <jallynkelley@gmail.com>
2017-12-13 20:19:34 -0600

    Everything done but diplay of JobID in URL

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -18,9 +19,10 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
+            
 
-            return View();
+            Job someJob = jobData.Find(id);
+            return View(someJob);
         }
 
         public IActionResult New()
@@ -32,10 +34,21 @@ namespace TechJobs.Controllers
         [HttpPost]
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
-            // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionType),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                };
+                jobData.Jobs.Add(newJob);
 
+                return View("Index", newJob);
+            }
+            
             return View(newJobViewModel);
         }
     }
